a8a18d2d73bc1d56283da4bc89f6cd6cbbfc565d
Vinicius Menezes <viny2999@gmail.com>
2022-08-26 18:02:36 -0300

    fix lint

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -2,51 +2,51 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 const swaggerDef = {
-  "openapi": "3.0.0",
-  "info": {
-    "description": "Swagger Documentation",
-    "version": "1.0.0",
-    "title": "Grupo SBF Backend Challenge"
+  openapi: '3.0.0',
+  info: {
+    description: 'Swagger Documentation',
+    version: '1.0.0',
+    title: 'Grupo SBF Backend Challenge'
   },
-  "servers": [{ "url": `${process.env.URL}/v1` }],
-  "host": `${process.env.URL}`,
-  "basePath": "/v1",
-  "paths": {
-    "/converter/{value}/{currency}": {
-      "get": {
-        "tags": [
-          "Currency Converter"
+  servers: [{ url: `${process.env.URL}/v1` }],
+  host: `${process.env.URL}`,
+  basePath: '/v1',
+  paths: {
+    '/converter/{value}/{currency}': {
+      get: {
+        tags: [
+          'Currency Converter'
         ],
-        "produces": ["application/json"],
-        "parameters": [
+        produces: ['application/json'],
+        parameters: [
           {
-            "name": "value",
-            "in": "path",
-            "description": "The Value of Product",
-            "required": true,
-            "type": "number"
+            name: 'value',
+            in: 'path',
+            description: 'The Value of Product',
+            required: true,
+            type: 'number'
           },
           {
-            "name": "currency",
-            "in": "path",
-            "description": "The Monetary Unit of a Country",
-            "required": true,
-            "type": "string"
+            name: 'currency',
+            in: 'path',
+            description: 'The Monetary Unit of a Country',
+            required: true,
+            type: 'string'
           }
         ],
-        "responses": {
-          "200": {
-            "description": "Successful Operation",
-            "schema": {
-              "type": "array"
+        responses: {
+          200: {
+            description: 'Successful Operation',
+            schema: {
+              type: 'array'
             }
           }
         }
       }
     }
   },
-  "definitions": {},
-  "components": {}
-}
+  definitions: {},
+  components: {}
+};
 
 export const SwaggerDefinition = swaggerDef;
